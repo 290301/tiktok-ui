eb01319274d71203f6a0803325472f7d17cc6d69
= <=>
2022-09-12 11:24:22 +0700

    New commit

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,23 @@
+//------import library
+import classNames from 'classnames/bind';
+
+//------import component
+import styles from './DefaultLayout.module.scss';
+import Header from '~/components/Layout/components/Header';
+import Sidebar from './Sidebar';
+
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+   return (
+      <div className={cx('wrapper')}>
+         <Header />
+         <div className={cx('container')}>
+            <Sidebar />
+            <div className={cx('content')}>{children}</div>
+         </div>
+      </div>
+   );
+}
+
+export default DefaultLayout;
